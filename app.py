a359cbcb808296355c0130e15e4785f68fb09a93
Shandy31 <66155371+Shandy31@users.noreply.github.com>
2021-11-27 20:06:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-	return render_template('home.html')
+	return render_template('Home.html')
 	# return "hello"
 @app.route('/predict', methods = ['POST'])
 def predict():
